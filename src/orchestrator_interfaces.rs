6e535557f08b261b7abe201fd84e46951cb76a27
Rivers Yang <rivers.yang@gmail.com>
2025-02-19 21:23:25 +0800

    Update orchestrator_interfaces.rs

--- src/orchestrator_interfaces.rs
+++ src/orchestrator_interfaces.rs
@@ -12,35 +12,48 @@ pub struct InvokeArgs {
 /// Invoke status code to be used in the response of invoke function,
 /// will be formatted as a string before returning to the caller
 ///
-/// 4xx - InvokeArgs Errors
-/// 5xx - Orchestrator Errors
-/// 7xx - Exchange Errors
+/// 3xx - Latency caused errors, may be retried
+/// 4xx - InvokeArgs errors
+/// 5xx - Orchestrator errors
+/// 7xx - Exchange errors
 #[derive(CandidType, Clone, Debug, Deserialize, Serialize, PartialEq, Eq)]
 pub enum InvokeStatus {
+    /// Transaction fee too low
+    _301(String),
     /// Invalid psbt_hex
     _401(String),
     /// Invalid psbt data
     _402(String),
-    /// Transaction fee too low
-    _403(String),
     /// Input UTXO already spent
     _404(String),
-    /// Invalid OP_RETURN data
-    _405(String),
     /// Invalid rune balance in psbt
     _406(String),
+    /// Missing intentions
+    _407,
+    /// Too many intentions
+    _408,
     /// Invalid intention
-    _407 {
+    _409 {
         intention_index: usize,
         error: String,
     },
     /// Intention mismatched with the psbt
-    _408 {
+    _410 {
         intention_index: usize,
         error: String,
     },
+    /// Invoke is paused
+    _501,
+    /// Orchestrator internal error
+    _502(String),
     /// Rune indexer not reachable
-    _501(String),
+    _503(String),
+    /// Rune indexer returned error
+    _504(String),
+    /// Invalid final tx
+    _505(String),
+    /// Invoke failed due to exchange error
+    _599,
     /// Exchange not reachable
     _701 {
         intention_index: usize,
@@ -65,33 +78,39 @@ pub type InvokeResponse = Result<String, String>;
 impl core::fmt::Display for InvokeStatus {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         match self {
+            InvokeStatus::_301(msg) => write!(f, "403 Transaction fee too low: {}", msg),
             InvokeStatus::_401(msg) => write!(f, "401 Invalid psbt hex: {}", msg),
             InvokeStatus::_402(msg) => write!(f, "402 Invalid psbt data: {}", msg),
-            InvokeStatus::_403(msg) => write!(f, "403 Transaction fee too low: {}", msg),
             InvokeStatus::_404(msg) => write!(f, "404 Input UTXO already spent: {}", msg),
-            InvokeStatus::_405(msg) => write!(f, "405 Invalid OP_RETURN data: {}", msg),
             InvokeStatus::_406(msg) => write!(f, "406 Invalid rune balance in psbt: {}", msg),
-            InvokeStatus::_407 {
+            InvokeStatus::_407 => write!(f, "407 Missing intentions"),
+            InvokeStatus::_408 => write!(f, "408 Too many intentions"),
+            InvokeStatus::_409 {
                 intention_index,
                 error,
             } => {
                 write!(
                     f,
-                    "407 Invalid intention: Intention index: {}, error: {}",
+                    "409 Invalid intention: Intention index: {}, error: {}",
                     intention_index, error
                 )
             }
-            InvokeStatus::_408 {
+            InvokeStatus::_410 {
                 intention_index,
                 error,
             } => {
                 write!(
                     f,
-                    "408 Intention mismatched with the psbt: Intention index: {}, error: {}",
+                    "410 Intention mismatched with the psbt: Intention index: {}, error: {}",
                     intention_index, error
                 )
             }
-            InvokeStatus::_501(msg) => write!(f, "501 Rune indexer not reachable: {}", msg),
+            InvokeStatus::_501 => write!(f, "501 Invoke is paused"),
+            InvokeStatus::_502(msg) => write!(f, "502 Orchestrator internal error: {}", msg),
+            InvokeStatus::_503(msg) => write!(f, "503 Rune indexer not reachable: {}", msg),
+            InvokeStatus::_504(msg) => write!(f, "504 Rune indexer returned error: {}", msg),
+            InvokeStatus::_505(msg) => write!(f, "505 Invalid final tx: {}", msg),
+            InvokeStatus::_599 => write!(f, "599 Invoke failed due to exchange error"),
             InvokeStatus::_701 {
                 intention_index,
                 error,
