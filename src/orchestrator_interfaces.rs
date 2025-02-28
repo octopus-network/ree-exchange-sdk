4a70e5a9f1bb34abaeb074386dc68d612f73a0f1
Rivers Yang <rivers.yang@gmail.com>
2025-02-28 09:32:25 +0800

    Revise InvokeStatus.

--- src/orchestrator_interfaces.rs
+++ src/orchestrator_interfaces.rs
@@ -20,6 +20,8 @@ pub struct InvokeArgs {
 pub enum InvokeStatus {
     /// Transaction fee too low
     _301(String),
+    /// Another invoke is in progress
+    _399,
     /// Invalid psbt_hex
     _401(String),
     /// Invalid psbt data
@@ -53,7 +55,7 @@ pub enum InvokeStatus {
     /// Invalid final tx
     _505(String),
     /// Invoke failed due to exchange error
-    _599,
+    _599(String),
     /// Exchange not reachable
     _701 {
         intention_index: usize,
@@ -78,13 +80,19 @@ pub type InvokeResponse = Result<String, String>;
 impl core::fmt::Display for InvokeStatus {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         match self {
-            InvokeStatus::_301(msg) => write!(f, "403 Transaction fee too low: {}", msg),
+            InvokeStatus::_301(msg) => {
+                write!(f, "301 Transaction fee too low: {}. Maybe try again.", msg)
+            }
+            InvokeStatus::_399 => write!(
+                f,
+                "399 Another invoke is in progress. Please try again later."
+            ),
             InvokeStatus::_401(msg) => write!(f, "401 Invalid psbt hex: {}", msg),
             InvokeStatus::_402(msg) => write!(f, "402 Invalid psbt data: {}", msg),
             InvokeStatus::_404(msg) => write!(f, "404 Input UTXO already spent: {}", msg),
             InvokeStatus::_406(msg) => write!(f, "406 Invalid rune balance in psbt: {}", msg),
-            InvokeStatus::_407 => write!(f, "407 Missing intentions"),
-            InvokeStatus::_408 => write!(f, "408 Too many intentions"),
+            InvokeStatus::_407 => write!(f, "407 Missing intentions."),
+            InvokeStatus::_408 => write!(f, "408 Too many intentions."),
             InvokeStatus::_409 {
                 intention_index,
                 error,
@@ -105,12 +113,16 @@ impl core::fmt::Display for InvokeStatus {
                     intention_index, error
                 )
             }
-            InvokeStatus::_501 => write!(f, "501 Invoke is paused"),
+            InvokeStatus::_501 => write!(f, "501 Invoke is paused. Please contact support."),
             InvokeStatus::_502(msg) => write!(f, "502 Orchestrator internal error: {}", msg),
             InvokeStatus::_503(msg) => write!(f, "503 Rune indexer not reachable: {}", msg),
             InvokeStatus::_504(msg) => write!(f, "504 Rune indexer returned error: {}", msg),
             InvokeStatus::_505(msg) => write!(f, "505 Invalid final tx: {}", msg),
-            InvokeStatus::_599 => write!(f, "599 Invoke failed due to exchange error"),
+            InvokeStatus::_599(msg) => write!(
+                f,
+                "599 Invoke failed due to exchange error. Please contact support with txid: {}",
+                msg
+            ),
             InvokeStatus::_701 {
                 intention_index,
                 error,
