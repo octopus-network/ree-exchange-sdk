bc36be184a7f4b22f586a4d649185f7930d0c190
Rivers Yang <rivers.yang@gmail.com>
2025-08-25 23:20:07 +0800

    Update orchestrator_interfaces.rs

--- types/src/orchestrator_interfaces.rs
+++ types/src/orchestrator_interfaces.rs
@@ -4,17 +4,11 @@ use serde::{Deserialize, Serialize};
 use std::str::FromStr;
 
 #[derive(CandidType, Clone, Debug, Deserialize, Serialize, PartialEq, Eq)]
-pub struct RegisterExchangeArgs {
-    pub exchange_id: String,
-    pub exchange_canister: Principal,
-    pub client_canisters: Vec<Principal>,
-}
-
-#[derive(CandidType, Clone, Debug, Deserialize, Serialize, PartialEq, Eq)]
 pub struct InvokeArgs {
     pub psbt_hex: String,
     pub intention_set: IntentionSet,
     pub initiator_utxo_proof: Vec<u8>,
+    pub client_info: Option<String>,
 }
 
 /// If successful, returns the txid of the transaction broadcasted,
